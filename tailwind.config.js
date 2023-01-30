becc588cfc99a9aa280327a2a479f89ced7e2320
entering-a-username <tamunacer@gmail.com>
2023-01-30 17:13:28 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "customsh": 'rgb(0,0,0,0.2) 0px 4px 12px',
+      }
+    },
+  },
+  plugins: [],
+}
